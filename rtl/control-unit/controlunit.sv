a60856aecbcd3aa63c345dc616c9da55701d159e
krivanikm <krivanikmatus@gmail.com>
2026-09-23 20:57:02 +0200

    starting to do first jmp logic

--- rtl/control-unit/controlunit.sv
+++ rtl/control-unit/controlunit.sv
@@ -4,13 +4,19 @@
 `define OP_MVIB  4'h3
 `define OP_LOAD  4'h4
 `define OP_STORE 4'h5
+`define OP_JMP   4'h6
 
 module controlunit (
     input  wire        clk,
     input  wire        rst,
     input  wire [15:0] rom_data,
     input wire [7:0] reg_read_data,
+    input wire z_flag,
+    input wire c_flag,
+    input wire n_flag,    
     output reg         pc_inc,
+    output reg         pc_load,
+    output reg [15:0]  pc_addr,
     output reg         write_reg_en,
     output reg  [3:0]  reg_addr,
     output reg         write_temp_from_reg,
@@ -35,14 +41,19 @@ module controlunit (
 
     S_STORE_ADDR,
     S_STORE_READ,
-    S_STORE_WRITE
+    S_STORE_WRITE,
+
+    S_PC_ADDR_REG_READ,
+    S_PC_ADDR_LOAD,
+
+
     }state_t;
 
     state_t state;
 
     reg [3:0] ir;        // Zmenené na 4-bit, keďže opcode má 4 bity
     reg [3:0] bytes_to_fetch;  
-    reg [7:0] temp_reg;
+    reg [15:0] temp_reg;
 
     // Prepojenie vnútorného stavu na výstup pre testbench
     assign state_out = state;
@@ -58,7 +69,7 @@ module controlunit (
             reg_data            <= 8'h00;
             write_temp_from_reg <= 1'b0;
             high_b              <= 1'b0;
-            temp_reg <= 8'h00;
+            temp_reg <= 16'h00;
         end else begin
             // predvolené hodnoty - pulzné signály trvajú 1 takt
             pc_inc              <= 1'b0;
@@ -84,6 +95,10 @@ module controlunit (
                             end
                         `OP_LOAD: state <= S_LOAD_ADDR; 
                         `OP_STORE: state <= S_STORE_ADDR;
+                        `OP_JMP:begin 
+                            state <= S_PC_ADDR_LOAD;
+                            pc_inc <= 1;
+                        end
                         default: state <= S_FETCH; 
                     endcase
                 end
@@ -102,7 +117,7 @@ module controlunit (
                     state        <= S_FETCH;
                 end
                 S_MOV_LATCH: begin
-                    temp_reg <= reg_read_data;
+                    temp_reg[7:0] <= reg_read_data;
                     reg_addr <= rom_data[11:8];
                     state <= S_MOV_WRITE;
                     pc_inc <= 1;
@@ -112,10 +127,14 @@ module controlunit (
                     reg_data <= temp_reg;
                     state <= S_FETCH;
                 end
+                S_PC_ADDR_REG_READ: begin 
+                    reg_addr<= rom_data[11:8];
+                    temp_reg[7:0] <= reg_read_data;
+                end
 
                 default: state <= S_FETCH;
             endcase
         end
     end
-
+temp_reg
 endmodule
